99d23a111c4f4eb914e95beeda044aa92b682db8
denisparaguayo <72168757+denisparaguayo@users.noreply.github.com>
2021-04-01 02:07:41 -0400

    pwa

--- js/scripts.js
+++ js/scripts.js
@@ -141,3 +141,20 @@ $(window).on('load', function () {
 
 
 });
+
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+          '/index.html',
+      
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
